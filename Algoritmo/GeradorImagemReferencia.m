4f18e07d195f135e9e43b218bc91078875176d61
hugoalkimim <hugodanilosantosalkimim@gmail.com>
2020-08-19 20:52:57 -0300

    Organiza codigo

--- Algoritmo/GeradorImagemReferencia.m
+++ Algoritmo/GeradorImagemReferencia.m
@@ -1,16 +1,18 @@
-v = VideoReader('vidF.mp4');
-
-v.CurrentTime = 59.0;
+v = VideoReader('videoF.mp4');
+v.CurrentTime = 0.0;
+% Resolução do video -> LarguraxAltura
+QNT_COLUNAS = 1080; % Largura do video
+QNT_LINHAS = 1920; % Altura do video
 
 qntFrames = 0;
-while hasFrame(v) && qntFrames < 150
+while hasFrame(v) && qntFrames <= 100
     qntFrames = qntFrames + 1
     frame = readFrame(v);
     M(qntFrames, :, :, :) = frame(:, :, :);
 end
 
-for i = 1:1920
-    for j = 1:1080
+for i = 1:QNT_LINHAS
+    for j = 1:QNT_COLUNAS
         M2(i, j, :) = median(M(:, i, j, :));
     end
 end
